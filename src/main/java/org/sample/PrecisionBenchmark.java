312e8f0b7363538aea1554a503d4574808dc9dcc
langurmonkey <me@tonisagrista.com>
2025-05-07 17:22:28 +0200

    feat: include instance creation overhead. Add complex functions like sin().

--- src/main/java/org/sample/PrecisionBenchmark.java
+++ src/main/java/org/sample/PrecisionBenchmark.java
@@ -1,92 +1,117 @@
 package org.sample;
 
 import org.apfloat.Apfloat;
+import org.apfloat.ApfloatMath;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.concurrent.TimeUnit;
-
-
+import ch.obermuhlner.math.big.*;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(value = 2)
-@Warmup(iterations = 2, time = 1)
-@Measurement(iterations = 2, time = 3)
+@Fork(value = 1)
+@Warmup(iterations = 1, time = 1)
+@Measurement(iterations = 1, time = 3)
 public class PrecisionBenchmark {
 
-    private static final int ITERATIONS = 100;
+  private static final int ITERATIONS = 100;
 
-    @State(Scope.Thread)
-    public static class BenchmarkState {
-        BigDecimal aBD;
-        BigDecimal bBD;
-        Apfloat aAF;
-        Apfloat bAF;
-        MathContext mc;
+  @State(Scope.Thread)
+  public static class BenchmarkState {
+    BigDecimal aBD;
+    BigDecimal bBD;
+    Apfloat aAF;
+    Apfloat bAF;
+    MathContext mc;
 
-        @Param({"25", "27", "30", "500", "1000"})  // Add different precision levels here
-        int precision;
+    @Param({ "25", "1000" }) // Add different precision levels here
+    int precision;
 
-        @Setup(Level.Trial)
-        public void setUp() {
-            mc = new MathContext(precision);
-            aBD = new BigDecimal("12345.6789012345678901234567890123456789", mc);
-            bBD = new BigDecimal("98765.4321098765432109876543210987654321", mc);
-            aAF = new Apfloat("12345.6789012345678901234567890123456789", precision);
-            bAF = new Apfloat("98765.4321098765432109876543210987654321", precision);
-        }
+    @Setup(Level.Trial)
+    public void setUp() {
+      mc = new MathContext(precision);
     }
+  }
 
-    @Benchmark
-    public void testBigDecimalAddition(BenchmarkState state, Blackhole bh) {
-        for (int i = 0; i < ITERATIONS; i++) {
-            BigDecimal result = state.aBD.add(state.bBD);
-            bh.consume(result);
-        }
+  @Benchmark
+  public void testBigDecimalAddition(BenchmarkState state, Blackhole bh) {
+    for (int i = 0; i < ITERATIONS; i++) {
+      var aBD = new BigDecimal("12345.6789012345678901234567890123456789", state.mc);
+      var bBD = new BigDecimal("98765.4321098765432109876543210987654321", state.mc);
+      var result = aBD.add(bBD);
+      bh.consume(result);
     }
+  }
 
-    @Benchmark
-    public void testApFloatAddition(BenchmarkState state, Blackhole bh) {
-        for (int i = 0; i < ITERATIONS; i++) {
-            Apfloat result = state.aAF.add(state.bAF);
-            bh.consume(result);
-        }
+  @Benchmark
+  public void testApFloatAddition(BenchmarkState state, Blackhole bh) {
+    for (int i = 0; i < ITERATIONS; i++) {
+      var aAF = new Apfloat("12345.6789012345678901234567890123456789", state.precision);
+      var bAF = new Apfloat("98765.4321098765432109876543210987654321", state.precision);
+      var result = aAF.add(bAF);
+      bh.consume(result);
     }
+  }
 
-    @Benchmark
-    public void testBigDecimalMultiplication(BenchmarkState state, Blackhole bh) {
-        for (int i = 0; i < ITERATIONS; i++) {
-            BigDecimal result = state.aBD.multiply(state.bBD);
-            bh.consume(result);
-        }
+  @Benchmark
+  public void testBigDecimalMultiplication(BenchmarkState state, Blackhole bh) {
+    for (int i = 0; i < ITERATIONS; i++) {
+      var aBD = new BigDecimal("12345.6789012345678901234567890123456789", state.mc);
+      var bBD = new BigDecimal("98765.4321098765432109876543210987654321", state.mc);
+      var result = aBD.multiply(bBD);
+      bh.consume(result);
     }
+  }
 
-    @Benchmark
-    public void testApFloatMultiplication(BenchmarkState state, Blackhole bh) {
-        for (int i = 0; i < ITERATIONS; i++) {
-            Apfloat result = state.aAF.multiply(state.bAF);
-            bh.consume(result);
-        }
+  @Benchmark
+  public void testApFloatMultiplication(BenchmarkState state, Blackhole bh) {
+    for (int i = 0; i < ITERATIONS; i++) {
+      var aAF = new Apfloat("12345.6789012345678901234567890123456789", state.precision);
+      var bAF = new Apfloat("98765.4321098765432109876543210987654321", state.precision);
+      var result = aAF.multiply(bAF);
+      bh.consume(result);
     }
+  }
 
-    @Benchmark
-    public void testBigDecimalDivision(BenchmarkState state, Blackhole bh) {
-        for (int i = 0; i < ITERATIONS; i++) {
-            BigDecimal result = state.aBD.divide(state.bBD, state.mc);
-            bh.consume(result);
-        }
+  @Benchmark
+  public void testBigDecimalDivision(BenchmarkState state, Blackhole bh) {
+    for (int i = 0; i < ITERATIONS; i++) {
+      var aBD = new BigDecimal("12345.6789012345678901234567890123456789", state.mc);
+      var bBD = new BigDecimal("98765.4321098765432109876543210987654321", state.mc);
+      var result = aBD.divide(bBD, state.mc);
+      bh.consume(result);
     }
+  }
 
-    @Benchmark
-    public void testApFloatDivision(BenchmarkState state, Blackhole bh) {
-        for (int i = 0; i < ITERATIONS; i++) {
-            Apfloat result = state.aAF.divide(state.bAF);
-            bh.consume(result);
-        }
+  @Benchmark
+  public void testApFloatDivision(BenchmarkState state, Blackhole bh) {
+    for (int i = 0; i < ITERATIONS; i++) {
+      var aAF = new Apfloat("12345.6789012345678901234567890123456789", state.precision);
+      var bAF = new Apfloat("98765.4321098765432109876543210987654321", state.precision);
+      var result = aAF.divide(bAF);
+      bh.consume(result);
     }
+  }
+
+  @Benchmark
+  public void testBigDecimalSin(BenchmarkState state, Blackhole bh) {
+    for (int i = 0; i < ITERATIONS; i++) {
+      var aBD = new BigDecimal("12345.6789012345678901234567890123456789", state.mc);
+      var result = BigDecimalMath.sin(aBD, state.mc);
+      bh.consume(result);
+    }
+  }
+
+  @Benchmark
+  public void testApfloatSin(BenchmarkState state, Blackhole bh) {
+    for (int i = 0; i < ITERATIONS; i++) {
+      var aBD = new Apfloat("12345.6789012345678901234567890123456789", state.precision);
+      var result = ApfloatMath.sin(aBD);
+      bh.consume(result);
+    }
+  }
 
 }
-
